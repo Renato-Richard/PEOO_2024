6aa9de547219a8abf01d2699872efb8b3b907906
RENATO RICHARD CÂMARA DA SILVA <20221011110022@ifrn.local>
2024-07-30 15:54:03 -0300

    mensagem

---
+++ 8-LISTAS/bingo.py
@@ -0,0 +1,13 @@
+import random
+class Bingo:
+    def __init__(self, numBolas: int):
+        self.__numBolas = numBolas
+        self.__sorteados = []
+    def Iniciar(self, numBolas: int):
+        if self.__numBolas > 0: self.__numBolas = numBolas
+        else: raise ValueError("Número inválido.")
+    # def Proximo(self):
+    #     if len(self.__sorteados)
+        n = random.randint(1, self.__numBolas)
+    def Sorteados(self):
+        return [Bingo.Proximo()]
