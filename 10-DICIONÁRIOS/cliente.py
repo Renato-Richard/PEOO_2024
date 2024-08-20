76536d8240221126a32d020ae1b4d35d98a1b69d
RENATO RICHARD CÂMARA DA SILVA <20221011110022@ifrn.local>
2024-08-20 15:57:32 -0300

    atualização

---
+++ 10-DICIONÁRIOS/cliente.py
@@ -0,0 +1,7 @@
+class Cliente:
+    def __init__(self, id: int, nome: str, email: str, fone: str):
+        self.__id = 0
+        self.__nome = ""
+        self.__email = ""
+        self.__fone = ""
+    
