0181581baafccda719334dafed90b0ef7fc9bba5
Renato-Richard <richard.renato@escolar.ifrn.edu.br>
2024-08-30 23:33:43 -0300

    update

--- 9-DATAS/Paciente.py
+++ 9-DATAS/Paciente.py
@@ -1,19 +1,16 @@
 from datetime import datetime
 class Paciente:
-    def __init__(self):
+    def __init__(self, n: str, c: str, t: str, nasc):
         self.__nome = ""
         self.__cpf = ""
         self.__telefone = ""
-        self.__nascimento = ""
-    def paciente(self, n: str, c: str, t: str, nasc: datetime):
+        self.__nascimento = nasc
         if self.__nome != "": self.__nome = n
         else: raise ValueError("O nome está vazio!")
         if self.__cpf != "": self.__cpf = c
         else: raise ValueError("O CPF está vazio!")
         if self.__telefone != "": self.__telefone = t
         else: raise ValueError("O número de telefone está vazio!")
-        if self.__nascimento != "": self.__nascimento = nasc
-        else: raise ValueError("A data de nascimento está vazia!")
     def idade(self, passado):
         presente = datetime.now()
         nascimento = datetime.strptime(passado, "%d/%m/%Y")
@@ -23,14 +20,27 @@ class Paciente:
 class UI:
     @staticmethod
     def menu():
-        print("Digite os dados do paciente:\n1: Nome\n2: CPF\n3: Telefone\n4: Nascimento")
+        print("Digite os dados do paciente:\n1: Dados\n2: Informações")
         return int(input())
     @staticmethod
     def main():
-        input("Informe o nome do paciente: ")
-        input("Informe o seu CPF: ")
-        input("Informe o seu telefone: ")
-        passado = input("Infome a sua data de nascimento: ")
         p = Paciente()
-        print(p.idade(passado))
+        op = -1
+        while op != 0:
+            op = UI.menu()
+            if op == 1: UI.dados()
+            if op == 2: UI.info(p)
+        print("Operação terminada.")
+    @staticmethod
+    def dados():
+        nome = input("Informe o nome do paciente: ")
+        cpf = input("Informe o seu CPF: ")
+        telefone = input("Informe o seu telefone: ")
+        nascimento = input("Infome a sua data de nascimento: ")
+        p = Paciente(nome, cpf, telefone, nascimento)
+        print(p.idade(nascimento))
+        return p
+    @staticmethod
+    def info(p):
+        print(p)
 UI.main()
